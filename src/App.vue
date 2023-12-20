c6b5f637dee87fcc955c7d8748f53a0780f2e881
Hector <hectorvallsmira2003@gmail.com>
2023-12-20 01:26:07 +0100

    npm run format por si acaso

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
 
-export default{
+export default {
   components: {
     RouterLink,
     RouterView
-  },
+  }
 }
 </script>
 
 <template>
   <header>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/logoBatoi.png" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Batoi Books</h1>
-  </div>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/logoBatoi.png" class="logo" alt="Vite logo" />
+      </a>
+      <h1>Batoi Books</h1>
+    </div>
     <div>
       <nav class="menu">
         <ul>
-        <li><RouterLink to="/">Ver Libros</RouterLink></li>
-        <li><RouterLink to="/form">Form</RouterLink></li>
-        <li><RouterLink to="/cart">Carrito</RouterLink></li>
+          <li><RouterLink to="/">Ver Libros</RouterLink></li>
+          <li><RouterLink to="/form">Form</RouterLink></li>
+          <li><RouterLink to="/cart">Carrito</RouterLink></li>
 
-        <li><RouterLink to="/about">About</RouterLink></li>
-      </ul>
+          <li><RouterLink to="/about">About</RouterLink></li>
+        </ul>
       </nav>
     </div>
   </header>
@@ -33,34 +33,31 @@ export default{
   <footer>Hector Valls Mira</footer>
 </template>
 
-
 <style>
 .menu {
-  position: fixed;  
-  top: 0;           
-  left: 0;          
-  width: 10%;     
-  height: 100%;     
-  background-color: #06a1f4; 
-  color: #fff;      
-  padding: 20px;    
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 10%;
+  height: 100%;
+  background-color: #06a1f4;
+  color: #fff;
+  padding: 20px;
   font-size: 26px;
 }
 
 .menu ul {
-  list-style: none; 
+  list-style: none;
   padding: 0;
   margin: 0;
 }
-
 
 .menu li {
   margin: 20px 0;
 }
 
 .menu a {
-  text-decoration: none; 
-  color: #fff;          
+  text-decoration: none;
+  color: #fff;
 }
-
 </style>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,48 +1,43 @@
 <script>
-import { store } from "@/store/store";
-import BooksRepository from "../repositories/books.repository.js";
-import AppBook from "./AppBook.vue";
-import AppMessages from "./AppMessages.vue";
-
+import { store } from '@/store/store'
+import BooksRepository from '../repositories/books.repository.js'
+import AppBook from './AppBook.vue'
+import AppMessages from './AppMessages.vue'
 
 export default {
   data() {
     return {
-      books: [],
+      books: []
     }
   },
-  components:{
+  components: {
     AppBook,
     AppMessages
   },
-  
-  
+
   async mounted() {
-    this.repository = new BooksRepository();
-    this.books = await this.repository.getAllBooks();
-    store.addMensaje("Libros cargados correctamente");
+    this.repository = new BooksRepository()
+    this.books = await this.repository.getAllBooks()
+    store.addMensaje('Libros cargados correctamente')
   },
 
   methods: {
-    async bookDel(id){
-      await this.repository.removeBook(id);
-      store.addMensaje("Libro con id " + id + " ha sido borrado de la base de datos");
-
+    async bookDel(id) {
+      await this.repository.removeBook(id)
+      store.addMensaje('Libro con id ' + id + ' ha sido borrado de la base de datos')
     }
-
   }
 }
 </script>
 
 <template>
-<app-messages></app-messages>
-<div class="list">
-  <app-book v-for="book in books" :key="book.id" :book="book"></app-book>
-</div>
+  <app-messages></app-messages>
+  <div class="list">
+    <app-book v-for="book in books" :key="book.id" :book="book"></app-book>
+  </div>
 </template>
 
 <style>
-
 .list {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
@@ -52,11 +47,10 @@ export default {
 }
 
 .list div {
-  margin-bottom: 20px; 
+  margin-bottom: 20px;
   padding: 10px;
   background-color: rgb(0, 119, 255);
 }
-
 
 @media (max-width: 480px) {
   #list {
